841bcb84ce4407212fc5a86124612915cab60b21
Aniketkhede <103448534+Aniketkhede@users.noreply.github.com>
2023-10-23 08:59:41 +0000

    dataTypes commit

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//alert(3+3) // we are using node js not browser
+
+
+// number
+//bigInt
+//string
+//boolean
+//undefined => type is undefined
+//null => type is object and it is standalone value
+//symbol => uniq
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 143341;
+
+let accountEmail = "aniket@gmail.com";
+
+var accountPassword= '12345';
+
+accountCity= "Jaipur";
+
+let xy;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,xy])
