f8384b9ea16cd7dcbfea1f43bc53a10553078e5c
Aniketkhede <103448534+Aniketkhede@users.noreply.github.com>
2023-10-23 12:44:46 +0000

    conversion and operation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,49 @@
+
+// let score = "33"
+// let ValueInNumber = Number(score)
+
+// let NotANumber = "33aniket"
+
+
+// let ValueInIntOfNotANumber = Number(NotANumber)
+
+
+// console.log(ValueInIntOfNotANumber) //=> NaN
+// console.log(Number(null))           //=> 0
+// console.log(Number(undefined))      //=> NaN
+
+
+
+// console.log(typeof undefined)       //  =>undefined
+// console.log(typeof null)            // => object
+// console.log(typeof NaN)             // => number
+// console.log(typeof true)            // => boolean
+// console.log(typeof false)           //=>boolean
+
+
+// "33"         =>33
+//"33abc"       => NaN
+// true         => 1; 
+//false         => 0 
+
+
+
+let isLoggedIn = 1;
+
+let ValueInBool = Boolean(isLoggedIn);
+let s = "aniket";
+let ValueInBoolfromString = Boolean(s)
+console.log(ValueInBoolfromString);
+
+
+// 1        => true
+// 0        => false
+// ""       =>false
+//"hitesh"   => true
+
+
+// Operations
+
+
+console.log(+true)
+console.log()
